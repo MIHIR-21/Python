12eb6c266c97ffdf2ab7769a1b087e049e1a816c
MIHIR-21 <trambadiyamihir635@gmail.com>
2024-06-08 20:12:27 +0200

    Palindrome Added

--- 02_palindrome.py
+++ 02_palindrome.py
@@ -1,9 +1,10 @@
-list = [1,2,3,2,1]
+list = list(map(int, input("Enter :").split()))
+
 
 list2 = list.copy()
 list2.reverse()
 
 if(list2 == list ):
-    print(list,"is palindrome")
+    print(list , "is pelindrome")
 else:
-    print(list,"is not palindrome")
+    print(list,"is not pelindrome")
