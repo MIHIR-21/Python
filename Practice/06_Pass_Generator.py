be45efe8675699bc535b08bb8edd8031cfb69108
MIHIR-21 <trambadiyamihir635@gmail.com>
2024-06-29 13:00:59 +0200

    Add files via upload
    Random password generator

---
+++ Practice/06_Pass_Generator.py
@@ -0,0 +1,12 @@
+import random
+import string
+
+pass_len = 12
+
+values= string.ascii_letters + string.digits + string.punctuation
+
+password = ""
+for i in range(pass_len):
+    password += random.choice(values)
+
+print("your password is:", password)
