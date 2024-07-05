02c7c178037700d86232798ae1856f21581edca1
Mihir-21 <trambadiyamihir635@gmail.com>
2024-07-05 16:21:15 +0200

    Update BasicLoops.py
    basics loops from 1 to 71

--- Pyramids/BasicLoops.py
+++ Pyramids/BasicLoops.py
@@ -34,7 +34,7 @@ def Loop5to8():
         for j in range(0, i+1):
             print(str[j], end=" ")
         print()
-
+      
     print("loop 6 : ")
     str = "ABCDE"
     for i in range(0,len(str)):
@@ -57,6 +57,7 @@ def Loop5to8():
         print()
 
 def Loop9to14():
+
     print("loop 9 :")
     a = 5
     for i in range(1, a+1):
@@ -110,6 +111,7 @@ def Loop15():
         print()
 
 def Loop16to19():
+
     print("loop 16 :")
     a = 5
     for i in range(a, 0, -1):
@@ -139,6 +141,7 @@ def Loop16to19():
         print()
 
 def Loop20to27():
+
     print("loop 20 : ")
     a = 5
     for i in range(a , 0, -1):
@@ -199,6 +202,7 @@ def Loop20to27():
         print()
 
 def Loop28to34():
+
     print("loop 28 : ")
     str = "abcde"
     for i in range(len(str)-1, -1 , -1):
@@ -255,7 +259,8 @@ def Loop28to34():
             print(str[i], end=" ")
         print()
 
-def Loop35to38():
+def Loop35to40():
+
     print("loop 35 : ")
     a = 5
     for i in range(1 , a+1):
@@ -287,7 +292,6 @@ def Loop35to38():
                 print("0", end=" ")
         print()
 
-def Loop39to47():
     print("loop 39 : ")
     a = 5
     for i in range(a , 0, -1):
@@ -301,6 +305,8 @@ def Loop39to47():
         for j in range(i , 0, -1):
             print(j, end=" ")
         print()
+
+def Loop41to45():
 
     print("loop 41 : ")
     a = 5
@@ -347,6 +353,8 @@ def Loop39to47():
             print(j, end=" ")
         print()
 
+def Loop46to53():
+
     print("loop 46 : ")
     a = 5 
     for i in range(a, 0 , -1):
@@ -365,7 +373,6 @@ def Loop39to47():
             print(1 if (j%2 == 0) else 0 , end=" ")
         print()
 
-def Loop48to52():
     print("loop 48 : ")
     str = " ABCDE"
     for i in range(1, len(str)):
@@ -393,12 +400,211 @@ def Loop48to52():
             print(str[i], end=" ")
         print()
 
+    print("loop 51 : ")
+    str = "abcde"
+    for i in range(len(str)-1, -1 , -1):
+        for j in range(i, 0, -1):
+            print(end="  ")
+        for j in range(i, len(str)):
+            print(str[i], end=" ")
+        print()
+
+    print("Loop 52 : ")
+    str = "ABCDE"
+    for i in range(len(str)-1, -1, -1):
+        for j in range(i, 0, -1):
+            print(end="  ")
+        for j in range(i, len(str)):
+            print(str[i], end=" ")
+        print()
+
+    print("Loop 53 : ")
+    a = 5
+    for i in range(1, a+1):
+        for j in range(i, a):
+            print(end="  ")
+        for j in range(i,0,-1):
+            print(i%2, end=" ")
+        print()
+
+def Loop54to61():
+    
+    print("loop 54 : ")
+    a = 5
+    for i in range(a+1, 1,-1):
+        for j in range(i,a+1):
+            print(end="  ")
+        for j in range(1, i ):
+            print(j, end=" ")
+        print()
+
+    print("loop 55 : ")
+    a  = 5
+    for i in range(1,a+1):
+        for j in range(1,i):
+            print(end="  ")
+        for j in range(a,i-1,-1):
+            print(j, end=" ")
+        print()
+
+    print("loop 56 : ")
+    a = 5
+    for i in range(1, a+1):
+        for j in range(1, i):
+            print(end="  ")
+        for j in range(i, a+1):
+            print(j, end=" ")
+        print()
+
+    print("loop 57 : ")
+    a = 5
+    for i in range(a, 0, -1):
+        for j in range(i,a):
+            print(end="  ")
+        for j in range(i, 0, -1):
+            print(j, end=" ")
+        print()
+
+    print("loop 58 : ")
+    a = 5
+    for i in range(a, 0, -1):
+        for j in range(i,a):
+            print(end="  ")
+        for j in range(i,0,-1):
+            print(i, end=" ")
+        print()
    
+    print("loop 59 : ")
+    a = 5
+    for i in range(1, a+1):
+        for j in range(1,i):
+            print(end="  ")
+        for j in range(i,a+1):
+            print(i,end=" ")
+        print()
+
+    print("loop 60 : ")
+    a = 5
+    for i in range(a, 0, -1 ):
+        for j in range(i,a):
+            print(end="  ")
+        for j in range(i,0,-1):
+            print(j%2,end=" ")
+        print()
+
+    print("loop 61 : ")
+    a = 5
+    for i in range(a, 0, -1):
+        for j in range(i,a):
+            print(end="  ")
+        for j in range(i,0,-1):
+            print(1 if(i %2 == 0) else 0, end=" ")
+        print()
+
+def Loop62to65():
+
+    print("loop 62 : ")
+    str = "abcde"
+    for i in range(len(str), 0 ,-1):
+        for j in range(i,len(str)):
+            print(end="  ")
+        for j in range(0, i ):
+            print(str[j], end=" ")
+        print()
+
+    print("loop 63 : ")
+    str = "ABCDE"
+    for i in range(len(str), 0 ,-1):
+        for j in range(i,len(str)):
+            print(end="  ")
+        for j in range(0, i ):
+            print(str[j], end=" ")
+        print()
+
+    print("loop 64 : ")
+    str = "abcde"
+    for i in range(0, len(str)):
+        for j in range(0,i):
+            print(end="  ")
+        for j in range(i, len(str)):
+            print(str[j], end=" ")
+        print()
+
+    print("loop 65 : ")
+    str = "ABCDE"
+    for i in range(0, len(str)):
+        for j in range(0,i):
+            print(end="  ")
+        for j in range(i, len(str)):
+            print(str[j], end=" ")
+        print()
+
+def Loop66to71():
+
+    print("loop 66 : ")
+    a = 5
+    for i in range(1, a+1):
+        for j in range(i,a):
+            print(end=" ")
+        for j in range(1,i+1):
+            print(j, end=" ")
+        print()
+
+    print("loop 67 : ")
+    str = "abcde"
+    for i in range(0, len(str)):
+        for j in range(i,len(str)-1):
+            print(end=" ")
+        for j in range(0,i+1):
+            print(str[j],end=" ")
+        print()
+    
+    print("loop 68 : ")
+    a = 5
+    for i in range(1,a+1):
+        for j in range(1,i+1):
+            print(end="* ")
+        print()
+
+    print("loop 69 : ")
+    a = 5
+    for i in range(1,a+1):
+        for j in range(i,a):
+            print(end=" ")
+        for j in range(1,i+1):
+            print(end="* ")
+        print()
+
+    print("loop 70 : ")
+    str = "ABCDE"
+    for i in range(0,len(str)):
+        for j in range(i,len(str)-1):
+            print(end=" ")
+        for j in range(0,i+1):
+            print(str[j], end=" ")
+        print()
+
+    print("loop 71 : ")
+    str = "abcde"
+    for i in range(len(str)-1,-1,-1):
+        for j in range(i,0,-1):
+            print(end=" ")
+        for j in range(len(str)-1, i-1,-1):
+            print(str[j] , end=" ")
+        print()
+
+
 # Loop1to4()
 # Loop5to8()
 # Loop9to14()
+# Loop15()
 # Loop16to19()
 # Loop20to27()
-# Loop35to38()
-# Loop39to47()
-Loop48to52()
+# Loop28to34()
+# Loop35to40()
+# Loop41to45()
+# Loop46to53()
+# Loop54to61()
+# Loop62to65()
+Loop66to71()
+
